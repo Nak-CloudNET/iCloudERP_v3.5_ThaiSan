7b785840f55e4a299dcba9d3d67ddd690100595b
chinroo <jeanroo94@gmail.com>
2018-07-16 08:51:03 +0700

    List POS (LIN)

--- themes/default/views/header.php
+++ themes/default/views/header.php
@@ -1131,20 +1131,43 @@
 		                                            </a>
 		                                        </li>
                                             </ul>
+
                                         </li>
-                                       
-                                        <!--<li id="pos_sales">
-                                            <a class="submenu" href="<?/*= site_url('pos/sales'); */?>">
-                                                <i class="fa fa-heart"></i>
-                                                <span class="text"> <?/*= lang('pos_sales'); */?></span>
+
+                                        <li id="pos_sales">
+                                            <a class="dropmenu" href="<?= site_url('pos/sales'); ?>">
+                                                <i class="fa fa-filter"></i>
+                                                <span class="text"> <?= lang('POS '); ?></span>
+                                                <span class="chevron closed"></span>
                                             </a>
-                                        </li>-->
-									<!--	<li id="add_pos_sales">
-                                        <a class="submenu" href="<?/*= site_url('pos'); */?>">
+                                            <ul>
+                                                <li>
+                                                    <a class="submenu" href="<?= site_url('pos/sales'); ?>">
+                                                        <i class="fa fa-heart"></i>
+                                                        <span class="text"> <?= lang('pos_sales'); ?></span>
+                                                    </a>
+                                                </li>
+                                                <li id="add_pos_sales" class="sub_navigation">
+                                                    <a class="submenu" href="<?= site_url('pos'); ?>">
+                                                        <i class="fa fa-plus-circle"></i>
+                                                        <span class="text"> <?= lang('add_pos_sale'); ?></span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </li>
+
+                                        <li id="pos_sales">
+                                            <a class="submenu" href="<?= site_url('pos/sales'); ?>">
+                                                <i class="fa fa-heart"></i>
+                                                <span class="text"> <?= lang('pos_sales'); ?></span>
+                                            </a>
+                                        </li>
+										<li id="add_pos_sales">
+                                        <a class="submenu" href="<?= site_url('pos'); ?>">
                                             <i class="fa fa-plus-circle"></i>
-                                            <span class="text"> <?/*= lang('add_pos_sale'); */?></span>
+                                            <span class="text"> <?= lang('add_pos_sale'); ?></span>
                                         </a>
-                                    	</li>-->
+                                    	</li>
                                     	<li id="sale_order_list_sale_order">
 											<a class="dropmenu" href="<?= site_url('sale_order/list_sale_order'); ?>">
 												<i class="fa fa-heart"></i>
