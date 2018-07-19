1d72fb07d04ed7a6ac8f8d5aa573ffcc749cf7cf
Nak <khat.ratanak@cloudnet.com.kh>
2018-07-19 16:19:09 +0700

    Nak upload

--- themes/default/views/sale_order/invoice_st_a5.php
+++ themes/default/views/sale_order/invoice_st_a5.php
@@ -16,7 +16,11 @@
     .container {
         width: 19.3cm;
     }
-
+    .title{
+        font-family:"Khmer OS Muol Light";
+        -mox-font-family:"Khmer OS Muol Light";
+        font-size: 15px;
+    }
     @page {
         size: 8.5in 11in;
         margin: 2%;
@@ -182,10 +186,10 @@
         </div>
 
         <div class="row">
-            <div class="col-sm-12 col-xs-12 inv" style="margin-top: -10px !important">
+            <div class="invoice" style="margin-top: -10px !important">
                 <center>
-                    <h4>វិក្កយបត្រ</h4>
-                    <h4 style="margin-top:-10px !important;">INVOICE</h4>
+                    <h4 class="title">វិក្កយបត្រ</h4>
+                    <h4 class="title" style="margin-top:-10px !important;">INVOICE</h4>
                 </center>
             </div>
         </div>
@@ -503,6 +507,7 @@
                 <p style="margin-left: 10px;margin-top:10px;"><?php echo strip_tags($invs->note); ?></p>
             </div>
         <?php } ?>
+        <br> <br> <br> <br>
     </div>	<!--div col sm 6 -->
 
     <div id="footer" class="row">
