863e144c3425cfc90827a5519468bfff5ee30ded
Nak <khat.ratanak@cloudnet.com.kh>
2018-08-01 11:45:51 +0700

    Nak upload

--- help/add_update_file.sql
+++ help/add_update_file.sql
@@ -5,12 +5,12 @@ ADD COLUMN `product_invoice`  varchar(255) NULL AFTER `product_noted`;
 
 ALTER TABLE `erp_sale_items`
 ADD COLUMN `product_invoice`  varchar(255) NULL AFTER `price_id`;
-ALTER TABLE iclouderp_v3_5_thai_san.erp_sale_order_items 
 
+ALTER TABLE erp_sale_order_items
 ADD COLUMN product_invoice varchar(255) NULL AFTER product_noted;
 
-ALTER TABLE `iclouderp_v3_5_thai_san`.`erp_sale_items` 
+ALTER TABLE `erp_sale_items`
 ADD COLUMN `amount_quantity` decimal(15, 0) NULL AFTER `quantity_received`;
 
-ALTER TABLE `iclouderp_v3_5_thai_san`.`erp_sale_order_items` 
+ALTER TABLE `erp_sale_order_items`
 ADD COLUMN `amount_quantity` decimal(15, 0) NULL AFTER `quantity_received`;
